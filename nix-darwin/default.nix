e324e8edca757e173111d8e4d73f5ae84edc19e5
Luke Amdor <luke.amdor@gmail.com>
2022-12-19 12:42:11 -0600

    wip

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -12,20 +12,20 @@ in {
   config = mkMerge [
     { home-manager.extraSpecialArgs.darwinConfig = config; }
     (mkIf (cfg.users != { }) {
-      # system.activationScripts.postActivation.text = concatStringsSep "\n"
-      #   (mapAttrsToList (username: usercfg: ''
-      #     echo Activating home-manager configuration for ${username}
-      #     sudo -u ${username} --set-home ${
-      #       pkgs.writeShellScript "activation-${username}" ''
-      #         ${lib.optionalString (cfg.backupFileExtension != null)
-      #         "export HOME_MANAGER_BACKUP_EXT=${
-      #           lib.escapeShellArg cfg.backupFileExtension
-      #         }"}
-      #         ${lib.optionalString cfg.verbose "export VERBOSE=1"}
-      #         exec ${usercfg.home.activationPackage}/activate
-      #       ''
-      #     }
-      #   '') cfg.users);
+      system.activationScripts.postActivation.text = concatStringsSep "\n"
+        (mapAttrsToList (username: usercfg: ''
+          echo Activating home-manager configuration for ${username}
+          sudo -u ${username} --set-home ${
+            pkgs.writeShellScript "activation-${username}" ''
+              ${lib.optionalString (cfg.backupFileExtension != null)
+              "export HOME_MANAGER_BACKUP_EXT=${
+                lib.escapeShellArg cfg.backupFileExtension
+              }"}
+              ${lib.optionalString cfg.verbose "export VERBOSE=1"}
+              exec ${usercfg.home.activationPackage}/activate
+            ''
+          }
+        '') cfg.users);
     })
   ];
 }
